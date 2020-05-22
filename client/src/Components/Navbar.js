ddc16e5c2bc623d97b77eb591910832c6bda99be
mona <hibaalaani@github.com>
2020-05-22 02:42:09 +0200

    fix color

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
 export default function Navbar() {
   return (
     <div>
-      <Nav className=" bg-info" justify variant="tabs" defaultActiveKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/home">Hiba Alaani </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">My Projects</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Contact</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      ;
+      <nav className="navbar bg-dark">
+        <h1>
+          <a href="index.html">
+            <i className="fas fa-code"></i> Hiba Al-anni
+          </a>
+        </h1>
+        <ul>
+          <li>
+            <a href="projects.html">My Projects</a>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
